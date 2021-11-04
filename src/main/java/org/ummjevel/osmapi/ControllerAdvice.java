b23ed4b7eea46226e87fd980f6bcb8297297e637
ummjevel <mseagle2023@gmail.com>
2021-11-04 12:33:32 +0900

    add empty body(array type)

--- src/main/java/org/ummjevel/osmapi/ControllerAdvice.java
+++ src/main/java/org/ummjevel/osmapi/ControllerAdvice.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.Array;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,7 +16,7 @@ public class ControllerAdvice {
 
         resBody.put("result", e.getResult());
         resBody.put("message", e.getMessage());
-        resBody.put("body", "");
+        resBody.put("body", new Array[0]);
 
         return new ResponseEntity<>(resBody, e.getStatus());
     }
